6fae7a15e1a250a2b51871dd3e313cd1ef4d12e0
Allan Wei <AWei@smithandlong.com>
2018-12-17 15:58:04 -0500

    first commit

---
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,16 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+#import <connectivity/ConnectivityPlugin.h>
+#import <flutter_mobile_vision/FlutterMobileVisionPlugin.h>
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FLTConnectivityPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTConnectivityPlugin"]];
+  [FlutterMobileVisionPlugin registerWithRegistrar:[registry registrarForPlugin:@"FlutterMobileVisionPlugin"]];
+}
+
+@end
